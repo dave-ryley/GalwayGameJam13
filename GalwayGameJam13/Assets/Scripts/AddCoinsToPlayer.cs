10ca84501fdb2c87ca4ef51c54a302bc1650b383
stephen121212 <44613641+stephen121212@users.noreply.github.com>
2019-06-15 18:04:19 +0100

    Adding score to the player

---
+++ GalwayGameJam13/Assets/Scripts/AddCoinsToPlayer.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AddCoinsToPlayer : MonoBehaviour
+{
+    public float coinValue = 1f;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+            collision.gameObject.GetComponent<Value_Collector>().ReceieveCoins(coinValue);
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ GalwayGameJam13/Assets/Scripts/Value_Collector.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Value_Collector : MonoBehaviour
+{
+    float coinsCollected = 0f;
+
+    public void ReceieveCoins(float amount)
+    {
+        coinsCollected += amount;
+    }
+
+    public String getTotalScore()
+    {
+        String score = ("Total score accumulated: " + coinsCollected);
+        return String;
+    }
+
+    void Update()
+    {
+        getTotalScore();
+    }
+}
