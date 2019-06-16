666fbc2c22946672cd3dbd1eb130803366c5d415
stephen121212 <44613641+stephen121212@users.noreply.github.com>
2019-06-16 10:20:09 +0100

    Update

--- GalwayGameJam13/Assets/Scripts/GGJInputManager.cs
+++ GalwayGameJam13/Assets/Scripts/GGJInputManager.cs
@@ -3,6 +3,8 @@ using System.Collections;
 
 public class GGJInputManager : MonoBehaviour
 {
+    boolean pressed = false;
+
     private string [] _inputs = new string[]
     {
         "q", "w", "e", "r", "t", "y", "u", "i", "o", "p", "a", "s", "d", "f", "g", "h", "j", "k", "l", "z", "x", "c", "v", "b", "n", "m"
@@ -10,13 +12,13 @@ public class GGJInputManager : MonoBehaviour
 
     void Update()
     {
-        for(int i = 0; i < _inputs.Length; i++)
-        {
-            if(Input.GetKeyDown(_inputs[i]))
+            for(int i = 0; i < _inputs.Length; i++)
             {
-                GGJGameManager.HandleInput(_inputs[i]);
+                if(Input.GetKeyDown(_inputs[i]))
+                {
+                    GGJGameManager.HandleInput(_inputs[i]);
+                }
             }
-        }
     }
 
 #region Public members
