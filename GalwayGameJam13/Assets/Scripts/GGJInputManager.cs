b96f4f67cfac1fed7384171a4b226c46ceddeaa3
stephen121212 <44613641+stephen121212@users.noreply.github.com>
2019-06-16 14:04:38 +0100

    Update to add images

--- GalwayGameJam13/Assets/Scripts/GGJInputManager.cs
+++ GalwayGameJam13/Assets/Scripts/GGJInputManager.cs
@@ -3,6 +3,7 @@ using System.Collections;
 
 public class GGJInputManager : MonoBehaviour
 {
+    
     private string [] _inputs = new string[]
     {
         "q", "w", "e", "r", "t", "y", "u", "i", "o", "p", "a", "s", "d", "f", "g", "h", "j", "k", "l", "z", "x", "c", "v", "b", "n", "m", "space"
